9b5f6cb54ea6a1bbbd1f255dd96f7ca118f2367c
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-18 13:11:24 +0530

    Update log_channel.py

--- haruka/modules/log_channel.py
+++ haruka/modules/log_channel.py
@@ -153,7 +153,6 @@ if is_module_loaded(FILENAME):
 - /logchannel: get log channel info
 - /setlog: set the log channel.
 - /unsetlog: unset the log channel.
-
 Setting the log channel is done by:
 - adding the bot to the desired channel (as an admin!)
 - sending /setlog in the channel
